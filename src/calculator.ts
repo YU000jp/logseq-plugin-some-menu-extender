510fed93f70845536c612bac2438e7688d38107f
YU <111847207+YU000jp@users.noreply.github.com>
2023-08-09 18:39:57 +0900

    remove evalExpression

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,36 +1,7 @@
-import { evalExpression } from '@hkh12/node-calc';
 import { BlockEntity } from '@logseq/libs/dist/LSPlugin.user';
 
+export function loadCalculator() {
 
-  //Prior art: https://github.com/hiway/logseq-calculator-plugin
-
-//calculator
-export async function calculatorMain(uuid) {
-  let Success: boolean = false;
-  const text = await logseq.Editor.getBlock(uuid, { includeChildren: false });
-  if (text) {
-    const result = await evalExpression(text.content);
-    if (result) {
-      await logseq.Editor.insertBlock(uuid, ` = ${result}`, { sibling: false, focus: true });
-      Success = true;
-      logseq.UI.showMsg("Success", "success");
-    }
-  }
-  if (Success === false) {
-    logseq.UI.showMsg("Failed", "error");
-  }
-}export function loadCalculator() {
-  logseq.Editor.registerBlockContextMenuItem("Block Calculator", async ({ uuid }) => {
-    calculatorMain(uuid);
-  });
-  logseq.Editor.registerSlashCommand("Block Calculator", async ({ uuid }) => {
-    calculatorMain(uuid);
-  });
-
-
-  //コマンドパレット `select blocks to calculate`
-  //選択したブロックの数値を合計して、最後のブロックに追記する
-  //バレッドのコンテキストメニューではブロックの複数選択ができないため
   logseq.App.registerCommandPalette({
     key: 'some-menu-extender-select-blocks-for-calculate',
     label: 'Select blocks to SUM (calculate)',
