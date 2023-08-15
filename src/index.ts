62b9e1d0c28bcf9bfa69802a3c1a2cbdbe538b3f
YU <111847207+YU000jp@users.noreply.github.com>
2023-08-15 10:51:12 +0900

    Cleanup

--- src/index.ts
+++ src/index.ts
@@ -11,41 +11,46 @@ import { loadCopyPageTitle } from "./CopyPageTitle";
 import { mainCSS } from "./mainCSS";
 
 const main = () => {
+  //設定画面の読み込み
   logseq.useSettingsSchema(settingsTemplate);
 
+  //CSSを読み込む
   mainCSS(logseq.baseInfo.id);
 
-  /* ContextMenuItem `repeat-task as LATER`  */
+  /* ContextMenuItem
+  `repeat-task as LATER`  */
   if (logseq.settings!.loadRepeatTask === true) loadRepeatTask();
 
-  /* ContextMenuItem `Copy block reference and embed`  */
+  /* ContextMenuItem
+  `Copy block reference and embed`  */
   if (logseq.settings!.loadReferenceEmbed === true) loadReferenceEmbed();
 
-  //コマンドパレット `select blocks to calculate`
+  //Slash command
+  //`Insert current page title as a link`
+  //現在のページタイトルをリンクとして挿入する
+  if (logseq.settings!.loadCurrentPageTitle === true) loadCurrentPageTitle();
+
+  //Page bar item
+  //ページ情報を表示する
+  if (logseq.settings!.loadPageInfo === true) loadPageInfo();
+
+  //In dropdown of 'create' item
+  //新規作成ドロップダウンメニューにボタンを追加
+  if (logseq.settings!.loadNewChildPageButton === true)
+    loadNewChildPageButton();
+
+  //Page title menu項目追加。ページタイトルをリンクとしてコピーする
+  if (logseq.settings!.loadCopyPageTitle === true) loadCopyPageTitle();
+
+  //コマンドパレット
+  //`select blocks to calculate`
   //選択したブロックの数値を合計して、最後のブロックに追記する
   //バレッドのコンテキストメニューではブロックの複数選択ができないため
   if (logseq.settings!.loadCalculator === true) loadCalculator();
-
-  //Slash command `Insert current page title as a link`
-  //現在のページタイトルをリンクとして挿入する
-  if(logseq.settings!.loadCurrentPageTitle === true)loadCurrentPageTitle();
-
-  //ページ情報を表示する
-  if(logseq.settings!.loadPageInfo === true)loadPageInfo();
-
-  //新規作成ドロップダウンメニューにボタンを追加
-  if(logseq.settings!.loadNewChildPageButton === true)loadNewChildPageButton();
-
-  //Page title menu項目追加。ページタイトルをリンクとしてコピーする
-  if(logseq.settings!.loadCopyPageTitle === true)loadCopyPageTitle();
-
-  //Rotate the task workflow state
+  //コマンドパレット
+  //`Rotate the task workflow state`
   //タスクのワークフロー状態を切り替える
   if (logseq.settings!.loadTaskWorkflowState === true) loadTaskWorkflowState();
-
-  // logseq.onSettingsChanged((newSet: LSPluginBaseInfo['settings'], oldSet: LSPluginBaseInfo['settings']) => {
-
-  // });
 }; /* end_main */
 
 logseq.ready(main).catch(console.error);
