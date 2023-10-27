bad445130805c29d0aa09d21494f369236691c09
YU <111847207+YU000jp@users.noreply.github.com>
2023-10-27 09:39:20 +0900

    Prettier

--- src/pageDateNotifier.ts
+++ src/pageDateNotifier.ts
@@ -1,4 +1,4 @@
-import { PageEntity } from "@logseq/libs/dist/LSPlugin";
+import { PageEntity } from "@logseq/libs/dist/LSPlugin"
 import { t } from "logseq-l10n"
 
 export const loadPageDateNotifier = () => {
@@ -8,7 +8,7 @@ export const loadPageDateNotifier = () => {
     template: `
         <div id="pageBar--pageInfoBarSpace" title=""></div>
         `,
-  });
+  })
   logseq.provideStyle(`
         div#root div#main-content-container{
         & div#pageBar--pageInfoBarSpace {
@@ -33,57 +33,55 @@ export const loadPageDateNotifier = () => {
         overflow:hidden;
         }
       }
-  `);
+  `)
 
   logseq.App.onPageHeadActionsSlotted(async () => {
-    setTimeout(() => insertPageBar(), 300);
-  });
+    setTimeout(() => insertPageBar(), 300)
+  })
   logseq.App.onRouteChanged(async () => {
-    setTimeout(() => insertPageBar(), 300);
-  });
-};
+    setTimeout(() => insertPageBar(), 300)
+  })
+}
 
 const insertPageBar = async () => {
   const elementPageBarSpace = parent.document.getElementById(
     "pageBar--pageInfoBarSpace"
-  ) as HTMLDivElement | null;
-  if (!elementPageBarSpace) return;
-  if (elementPageBarSpace.dataset.pageInfoCheck) return;
-  const current = (await logseq.Editor.getCurrentPage()) as PageEntity | null;
-  if (!current) return;
-  if (!current.updatedAt && !current.createdAt) return;
-  const updated: Date = new Date(current.updatedAt as number);
+  ) as HTMLDivElement | null
+  if (!elementPageBarSpace) return
+  if (elementPageBarSpace.dataset.pageInfoCheck) return
+  const current = (await logseq.Editor.getCurrentPage()) as PageEntity | null
+  if (!current) return
+  if (!current.updatedAt && !current.createdAt) return
+  const updated: Date = new Date(current.updatedAt as number)
   //updatedをフォーマットする(最後の3文字を削除する)
   const updatedString =
-    dateFormatter.format(updated) + " " + timeFormatter.format(updated);
-  const created: Date = new Date(current.createdAt as number);
+    dateFormatter.format(updated) + " " + timeFormatter.format(updated)
+  const created: Date = new Date(current.createdAt as number)
   //createdをフォーマットする
   const createdString =
-    dateFormatter.format(created) + " " + timeFormatter.format(created);
-  elementPageBarSpace.innerHTML = `<table>${
-    updatedString
-      ? `<tr title="last updated"><th>${t("updated-at")}</th><td>` +
-        updatedString +
-        "</td></tr>"
+    dateFormatter.format(created) + " " + timeFormatter.format(created)
+  elementPageBarSpace.innerHTML = `<table>${updatedString
+    ? `<tr title="last updated"><th>${t("updated-at")}</th><td>` +
+    updatedString +
+    "</td></tr>"
+    : ""
+    }${logseq.settings!.pageDateNotifierCreatedAt === true && updated !== created && createdString
+      ? `<tr title="${t("⚠️After running 're-index'")}"><th>${t("created-at")}</th><td>` +
+      createdString +
+      "</td></tr>"
       : ""
-  }${
-    logseq.settings!.pageDateNotifierCreatedAt === true && updated !== created && createdString
-        ? `<tr title="${t("⚠️After running 're-index'")}"><th>${t("created-at")}</th><td>` +
-          createdString +
-          "</td></tr>"
-      : ""
-  }</table>`;
-  elementPageBarSpace.dataset.pageInfoCheck = "true";
-};
+    }</table>`
+  elementPageBarSpace.dataset.pageInfoCheck = "true"
+}
 
 // Intl.DateTimeFormatオブジェクトを作成（デフォルトロケールを使用）
 export const dateFormatter = new Intl.DateTimeFormat("default", {
   year: "numeric",
   month: "long",
   day: "numeric",
-});
+})
 
 export const timeFormatter = new Intl.DateTimeFormat("default", {
   hour: "numeric",
   minute: "numeric",
-});
+})
